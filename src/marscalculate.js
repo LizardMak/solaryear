2183b5b84819d6a778543a781f51ff98b0ee77bc
Hunter Empey <LizardMak@gmail.com>
2023-11-13 16:07:19 -0700

    marsBirthdayCountdown test1 red (real)

--- src/marscalculate.js
+++ src/marscalculate.js
@@ -2,6 +2,5 @@
 export function marsBirthdayCountdown(currentAge, targetAge) {
 
     const yearDiff = targetAge - currentAge;
-    const mercuryCountdown = yearDiff / 1.88;
-    return mercuryCountdown + " years until birthday!";
+  
 }
