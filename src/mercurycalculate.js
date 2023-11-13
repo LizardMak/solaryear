7be07492c821a7c59aa9857ea2e617a306c83754
Hunter Empey <LizardMak@gmail.com>
2023-11-13 11:49:43 -0700

    mercuryBirthdayCountdown test2 green

--- src/mercurycalculate.js
+++ src/mercurycalculate.js
@@ -1,6 +1,10 @@
 
 export function mercuryBirthdayCountdown(currentAge, targetAge) {
+  if (currentAge > targetAge) {
+    return "error";
+  } else {
   const yearDiff = targetAge - currentAge;
   const mercuryCountdown = yearDiff / .24;
   return mercuryCountdown;
+  };
 }
