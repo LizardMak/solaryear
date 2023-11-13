c42b58263bae23076226af822ae9640e42eef38e
Hunter Empey <LizardMak@gmail.com>
2023-11-13 16:32:58 -0700

    add imports to index

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import './mercurycalculate.js';
 import './inputdata.js';
+import './jupitercalculate.js';
+import './marscalculate.js';
+import './venuscalculate.js'
