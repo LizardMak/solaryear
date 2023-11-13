8014d18b1117c12a098d021d635ca79e573c8831
Hunter Empey <LizardMak@gmail.com>
2023-11-13 16:10:09 -0700

    marsBirthdayCountdown test2 red

--- __tests__/marscalculate.test.js
+++ __tests__/marscalculate.test.js
@@ -10,4 +10,11 @@ describe('marsBirthdayCountdown', () => {
     const marsYearsUntil = marsBirthdayCountdown(userData.inputAge, userData.inputTargetAge);
     expect(marsYearsUntil).toEqual("5.319148936170213 years until birthday!")
   }) 
+  test('It should calculate how many years ago a birthday was if the current age is larger than the target', () => {
+    const userData = new userInputs();
+    userData.inputAge = 31;
+    userData.inputTargetAge = 21;
+    const marsYearsUntil = marsBirthdayCountdown(userData.inputAge, userData.inputTargetAge);
+    expect(marsYearsUntil).toEqual("birthday was 5.319148936170213 years ago!");
+  })
 })
