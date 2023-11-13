9448be3c5be173d7259bc9797f4ec2ef5f3e1291
Hunter Empey <LizardMak@gmail.com>
2023-11-13 15:19:35 -0700

    venusBirthdayCountdown test2 green

--- src/venuscalculate.js
+++ src/venuscalculate.js
@@ -2,5 +2,11 @@
 export function venusBirthdayCountdown(currentAge, targetAge) {
   const yearDiff = targetAge - currentAge;
   const venusCountdown = yearDiff / .62;
+  if (venusCountdown >= 0) {
   return venusCountdown + " years until birthday!"
+  } else {
+    const venusNumString = venusCountdown.toString();
+    const venusPastString = venusNumString.replace("-", "");
+    return "birthday was " + venusPastString + " years ago!"
+  }
 }
