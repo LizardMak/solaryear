acf6232f86e11d872320d68a98a5c3ae9abcdb1f
Hunter Empey <LizardMak@gmail.com>
2023-11-14 18:32:03 -0700

    add current result UI

--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,5 @@
+import { UserInputs } from "./inputdata";
+
 window.addEventListener("load", function() {
   this.window.document.getElementById("form").addEventListener("submit", handleSubmit)
 })
@@ -5,7 +7,26 @@ window.addEventListener("load", function() {
 function handleSubmit() {
   event.preventDefault();
   if (document.getElementById("inputCurrentAge").value == "") {
+    document.getElementById("resultsCurrent").setAttribute("class", "hidden")
     document.getElementById("errorMessage1").setAttribute("class", "red");
     document.getElementById("errorMessage2").setAttribute("class", "hidden");
+  } else if (document.getElementById("inputCurrentAge").value < 0 || document.getElementById("inputTargetAge").value < 0) {
+    document.getElementById("resultsCurrent").setAttribute("class", "hidden")
+    document.getElementById("errorMessage1").setAttribute("class", "hidden");
+    document.getElementById("errorMessage2").setAttribute("class", "red");
+  } else if (document.getElementById("inputTargetAge").value == "") {
+    document.getElementById("errorMessage1").setAttribute("class", "hidden");
+    document.getElementById("errorMessage2").setAttribute("class", "hidden");
+    const userCurrentAge = document.getElementById("inputCurrentAge").value;
+    const userData = new UserInputs(userCurrentAge, null);
+    const currentJupiter = userData.jupiterMathCurrent();
+    const currentMars = userData.marsMathCurrent();
+    const currentMercury = userData.mercuryMathCurrent();
+    const currentVenus = userData.venusMathCurrent();
+    document.getElementById("jupiterOutput").innerHTML = currentJupiter;
+    document.getElementById("marsOutput").innerHTML = currentMars;
+    document.getElementById("mercuryOutput").innerHTML = currentMercury;
+    document.getElementById("venusOutput").innerHTML = currentVenus;
+    document.getElementById("resultsCurrent").removeAttribute("class")
   }
 }
